398863a3458ee4b192894032a4c9bbe5e599f62b
YeyoM <yeyomoreno2003@hotmail.com>
2025-04-07 08:53:31 -0600

    feat: :sparkles: Read data from firebase

--- src/main_report.py
+++ src/main_report.py
@@ -4,15 +4,13 @@ from distribucion_trastornos import reporte_distribucion_trastornos
 from edad_trastorno import reporte_distribucion_edades_trastornos
 from estado_origen_trastorno import reporte_distribucion_estado_origen_trastorno
 from estado_residencia_trastorno import reporte_distribucion_estado_residencia_trastorno
-from sexo_trastorno import reporte_distribucion_sexo_trastornos
 from factores_en_trastornos import reporte_caracteristicas_asociadas
-import descargar_reporte
+from get_data import get_firebase_data
+from sexo_trastorno import reporte_distribucion_sexo_trastornos
 
-path = "false_data/jtest_data.json"
-# Ejecutar todos los archivos
-# Excepto "descargar reporte.py", en lo que encuentro una mejor manera de hacer la descarga.
 if __name__ == "__main__":
-    crear_tabla_principal(path)
+    data = get_firebase_data()
+    crear_tabla_principal(data)
     reporte_distribucion_edades_trastornos()
     reporte_distribucion_sexo_trastornos()
     reporte_distribucion_estado_origen_trastorno()
