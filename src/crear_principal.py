3b2479ae48fc2524fa53fb1b0035ed1d6efb0485
YeyoM <yeyomoreno2003@hotmail.com>
2025-04-07 21:18:01 -0600

    feat: :sparkles: Read data from firebase

--- src/crear_principal.py
+++ src/crear_principal.py
@@ -1,43 +1,37 @@
-import openpyxl, json, os
-from openpyxl.styles import Alignment, Border, Font, PatternFill, Side
-from form_tabla import crear_tabla
 from datetime import datetime
 
+import openpyxl
+from openpyxl.styles import Alignment, Border, Font, PatternFill, Side
+
+from form_tabla import crear_tabla
+
+
 # Función para calcular la edad de las personas con su fecha de nacimiento
-def calcular_edad(fecha_nacimiento_str):
-    nacimiento = datetime.strptime(fecha_nacimiento_str, "%d-%m-%Y")
+def calcular_edad(fecha_nacimiento):
+    if not fecha_nacimiento or type(fecha_nacimiento) == str:
+        return ""
     hoy = datetime.today()
 
-    edad = hoy.year - nacimiento.year
-    if (hoy.month, hoy.day) < (nacimiento.month, nacimiento.day):
-        edad -= 1
+    edad = (
+        hoy.year
+        - fecha_nacimiento.year
+        - ((hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
+    )
 
     return edad
 
+
 # Crear la tabla principal, donde se muestran los resultados de la encuesta y sus datos relevantes.
 # No se incluyeron las respuestas de cada pregunta.
-# La tabla generado aqui se utilizara para generar el resto de reportes, para no tener que estar 
+# La tabla generado aqui se utilizara para generar el resto de reportes, para no tener que estar
 # llamando a la base de datos en cada reporte.
-def crear_tabla_principal(path="false_data/jtest_data.json"):
-
-    if not os.path.exists(path):
-        print("Error: El archivo no existe.")
-        return
-
-    # Leer el archivo JSON
-    try:
-        with open(path, "r", encoding="utf-8") as archivo:
-            data = json.load(archivo)
-    except Exception as e:
-        print(f"Error al leer el archivo: {e}")
-        return
-
+def crear_tabla_principal(data):
     # Obtener los datos de cadda relevantes
     lineas = []
     for persona in data:
         fecha_nacimiento = persona.get("birthdate")
         try:
-            edad = calcular_edad(fecha_nacimiento)
+            edad = calcular_edad(fecha_nacimiento) if fecha_nacimiento else ""
         except ValueError:
             edad = ""
         fila = [
@@ -78,7 +72,7 @@ def crear_tabla_principal(path="false_data/jtest_data.json"):
             persona.get("diagnosticN1", ""),
             persona.get("diagnosticN2", ""),
             persona.get("diagnosticO1", ""),
-            persona.get("diagnosticP1", "")
+            persona.get("diagnosticP1", ""),
         ]
         lineas.append(fila)
 
@@ -89,55 +83,64 @@ def crear_tabla_principal(path="false_data/jtest_data.json"):
 
     # Definir encabezados de la tabla
     encabezados = [
-        "Nombre", 
-        "Edad", 
-        "Sexo", 
-        "Preferencia sexual", 
-        "Estado de origen", 
+        "Nombre",
+        "Edad",
+        "Sexo",
+        "Preferencia sexual",
+        "Estado de origen",
         "Estado de residencia",
     ]
 
     trastornos = [
-        "Episodio depresivo mayor actual", 
-        "Episodio depresivo mayor recidivante", 
-        "Episodio depresivo mayor con síntomas melancólicos actual", 
-        "Trastorno distímico actual", 
-        "Riesgo de suicidio", "Riesgo", 
-        "Episodio hipomaníaco", "Periodo de episodio hipomaníaco", 
-        "Episodio maníaco", "Periodo de episodio maníaco", 
-        "Trastorno de angustia de por vida", "Periodo de trastorno de angustia", 
-        "Crisis actual con síntomas limitados", "Periodo de crisis", 
-        "Trastorno de angustia actual", 
-        "Trastorno de angustia sin agorafobia actual", 
-        "Trastorno de angustia con agorafobia actual", 
-        "Agorafobia actual sin historial de trastorno de angustia", 
-        "Fobia social actual", 
-        "Estado por estrés postraumático actual", 
-        "Dependencia de alcohol actual", 
-        "Abuso de alcohol actual", 
-        "Dependencia de sustancias actual", 
-        "Abuso de sustancias actual", 
-        "Trastorno psicótico actual", 
-        "Trastorno psicótico de por vida", 
-        "Trastorno del estado de ánimo con síntomas psicóticos actual", 
-        "Anorexia nerviosa actual", 
-        "Bulimia nerviosa actual", 
-        "Anorexia nerviosa tipo compulsivo/purgativo actual", 
-        "Trastorno de ansiedad generalizada actual", 
-        "Trastorno antisocial de la personalidad de por vida"
+        "Episodio depresivo mayor actual",
+        "Episodio depresivo mayor recidivante",
+        "Episodio depresivo mayor con síntomas melancólicos actual",
+        "Trastorno distímico actual",
+        "Riesgo de suicidio",
+        "Riesgo",
+        "Episodio hipomaníaco",
+        "Periodo de episodio hipomaníaco",
+        "Episodio maníaco",
+        "Periodo de episodio maníaco",
+        "Trastorno de angustia de por vida",
+        "Periodo de trastorno de angustia",
+        "Crisis actual con síntomas limitados",
+        "Periodo de crisis",
+        "Trastorno de angustia actual",
+        "Trastorno de angustia sin agorafobia actual",
+        "Trastorno de angustia con agorafobia actual",
+        "Agorafobia actual sin historial de trastorno de angustia",
+        "Fobia social actual",
+        "Estado por estrés postraumático actual",
+        "Dependencia de alcohol actual",
+        "Abuso de alcohol actual",
+        "Dependencia de sustancias actual",
+        "Abuso de sustancias actual",
+        "Trastorno psicótico actual",
+        "Trastorno psicótico de por vida",
+        "Trastorno del estado de ánimo con síntomas psicóticos actual",
+        "Anorexia nerviosa actual",
+        "Bulimia nerviosa actual",
+        "Anorexia nerviosa tipo compulsivo/purgativo actual",
+        "Trastorno de ansiedad generalizada actual",
+        "Trastorno antisocial de la personalidad de por vida",
     ]
 
     encabezados += trastornos
 
-    # Índices de los trastornos 
+    # Índices de los trastornos
     indice_trastornos = list(range(7, len(encabezados) + 1))
 
     # Estilos para encabezados
     header_font = Font(bold=True, color="FFFFFF")
     header_fill = PatternFill(start_color="4F81BD", fill_type="solid")
     header_alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
-    thin_border = Border(left=Side(style="medium"), right=Side(style="medium"),
-                         top=Side(style="medium"), bottom=Side(style="medium"))
+    thin_border = Border(
+        left=Side(style="medium"),
+        right=Side(style="medium"),
+        top=Side(style="medium"),
+        bottom=Side(style="medium"),
+    )
 
     # Agregar encabezados
     for col_num, encabezado in enumerate(encabezados, 1):
@@ -150,17 +153,25 @@ def crear_tabla_principal(path="false_data/jtest_data.json"):
     # Agregar los datos a la tabla
     for row_num, fila in enumerate(lineas, 2):
         if len(fila) != len(encabezados):
-            fila += [""] * (len(encabezados) - len(fila))  # En caso de no haber dato se rellena con un vacio
-        
+            fila += [""] * (
+                len(encabezados) - len(fila)
+            )  # En caso de no haber dato se rellena con un vacio
+
         # Contar cantidad de trastornos del entrevistado actual
-        cantidad_si = sum(1 for i in indice_trastornos if i - 1 < len(fila) and fila[i - 1].strip() != "")
-        
+        cantidad_si = sum(
+            1
+            for i in indice_trastornos
+            if i - 1 < len(fila) and fila[i - 1].strip() != ""
+        )
+
         # Definir color en función para darle al usuario en funcion de la cantidad de trastornos que padece
         if cantidad_si > 0:
-            rojo = 255  
-            naranja = max(0, 255 - (cantidad_si * 25))  # Disminuye progresivamente de 255 a 0
+            rojo = 255
+            naranja = max(
+                0, 255 - (cantidad_si * 25)
+            )  # Disminuye progresivamente de 255 a 0
 
-            color_hex = f"{rojo:02X}{naranja:02X}00"  
+            color_hex = f"{rojo:02X}{naranja:02X}00"
             fill_color = PatternFill(start_color=color_hex, fill_type="solid")
         else:
             # No se pone color a usuarios que no sufren ningun trastorno
@@ -169,14 +180,16 @@ def crear_tabla_principal(path="false_data/jtest_data.json"):
         for col_num, dato in enumerate(fila, 1):
             valor = "Si" if dato == encabezados[col_num - 1] else dato
             celda = ws.cell(row=row_num, column=col_num, value=valor)
-            celda.alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
+            celda.alignment = Alignment(
+                horizontal="center", vertical="center", wrap_text=True
+            )
             celda.border = thin_border
             # Aplicar color de acuerdo a la cantidad de trastornos al nombre del entrevistado
-            if fill_color and col_num == 1:  
+            if fill_color and col_num == 1:
                 celda.fill = fill_color
             # Se pintan de amarillos los "Si", para identificar más facilmente los trastornos padecidos
             if valor == "Si":
-                celda.fill = PatternFill(start_color="FFD700", fill_type="solid") 
+                celda.fill = PatternFill(start_color="FFD700", fill_type="solid")
             if valor == "Bajo":
                 celda.fill = PatternFill(start_color="FFD700", fill_type="solid")
             if valor == "Moderado":
@@ -190,14 +203,19 @@ def crear_tabla_principal(path="false_data/jtest_data.json"):
 
     # Ajustar el ancho de las columnas
     for col in range(1, len(encabezados) + 1):
-        max_length = max(len(str(ws.cell(row=row, column=col).value or "")) for row in range(1, len(lineas) + 2))
-        ws.column_dimensions[openpyxl.utils.get_column_letter(col)].width = min(max_length + 2, 30)
+        max_length = max(
+            len(str(ws.cell(row=row, column=col).value or ""))
+            for row in range(1, len(lineas) + 2)
+        )
+        ws.column_dimensions[openpyxl.utils.get_column_letter(col)].width = min(
+            max_length + 2, 30
+        )
 
     # Ajustar altura de filas
     for row in range(1, len(lineas) + 2):
         ws.row_dimensions[row].height = 40 if row == 1 else 20
 
-    # Aplicar formato de tabla 
+    # Aplicar formato de tabla
     crear_tabla(ws, type_table="TableStyleLight11", table_name="Tabla_general")
     # Guardar el archivo Excel
     output_path = "reporte_principal.xlsx"
